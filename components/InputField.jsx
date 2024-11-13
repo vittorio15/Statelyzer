1ec10be7c63aa7bd47e1f58a8dd9ff46849d64d8
vittorio15 <viktororgovan20030515@gmail.com>
2024-11-13 18:02:59 +0100

    label fills and skeletonloader improvements

--- components/InputField.jsx
+++ components/InputField.jsx
@@ -17,7 +17,7 @@ const InputField = () => {
 
   return (
     <input
-      className="px-4 py-2 rounded-2xl border-2 border-gray-300 hover:border-primary focus:border-primary focus:outline-none focus:ring-2 focus:ring-primary"
+      className="px-4 py-2 text-center rounded-2xl border-2 border-gray-300 hover:border-primary focus:border-primary focus:outline-none focus:ring-2 focus:ring-primary"
       value={value}
       maxLength={11}
       onChange={handleChange}
--- components/InputTable.jsx
+++ components/InputTable.jsx
@@ -15,15 +15,20 @@ const InputTable = () => {
   }, []);
 
   const SkeletonLoader = () => (
-    <div className="w-full my-2 animate-pulse space-y-4 bg-gradient-to-r from-primary via-green-500 to-lime-400 shadow-2xl rounded-2xl">
-      <div className="ml-2 mr-2 my-4 h-6 bg-gray-200 rounded w-1/3"></div>
-      <div className="ml-2 mr-2 my-4 h-6 bg-gray-200 rounded w-1/3"></div>
-      <div className="ml-2 mr-2 h-6 bg-gray-200 rounded w-full"></div>
-      <div className="ml-2 mr-2 h-6 bg-gray-200 rounded w-2/3"></div>
-      <div className="ml-2 mr-2 h-6 bg-gray-200 rounded w-full"></div>
-      <div className="ml-2 mr-2 my-4 h-6 bg-gray-200 rounded w-1/4"></div>
+    <div className="mt-6 w-full my-2 animate-pulse bg-gradient-to-r from-primary via-green-500 to-lime-400 shadow-2xl rounded-2xl grid place-items-center">
+      <div className="my-2 h-12 bg-gray-200 rounded-2xl w-3/4"></div>
+      <div className="my-2 h-12 bg-gray-200 rounded-2xl w-1/2"></div>
+      <div className="my-2 h-12 bg-gray-200 rounded-2xl w-2/3"></div>
+      <div className="my-2 h-12 bg-gray-200 rounded-2xl w-4/5"></div>
+      <div className="my-2 h-12 bg-gray-200 rounded-2xl w-3/5"></div>
+      <div className="my-2 h-12 bg-gray-200 rounded-2xl w-2/5"></div>
+      <div className="my-2 h-12 bg-gray-200 rounded-2xl w-1/2"></div>
+      <div className="my-2 h-12 bg-gray-200 rounded-2xl w-5/6"></div>
+      <div className="my-2 h-12 bg-gray-200 rounded-2xl w-3/4"></div>
+      <div className="my-2 h-12 bg-gray-200 rounded-2xl w-1/3"></div>
     </div>
   );
+  
 
   const handleButtonClick = (inputType) => {
     setSelectedInput(inputType);
@@ -41,19 +46,19 @@ const InputTable = () => {
       <div className="flex space-x-4 justify-center ml-10 w-8/12 mb-2">
         <button
           onClick={() => handleButtonClick('type1')}
-          className={`px-6 py-2 rounded-md ${selectedInput === 'type1' ? 'bg-gradient-to-r from-primary via-green-500 to-lime-400 text-white' : 'bg-gray-200 text-gray-600'}`}
+          className={`px-6 py-2 rounded-md hover:scale-105 ${selectedInput === 'type1' ? 'bg-gradient-to-r from-primary via-green-500 to-lime-400 text-white' : 'bg-gray-200 text-gray-600'}`}
         >
           Basic
         </button>
         <button
           onClick={() => handleButtonClick('type2')}
-          className={`px-6 py-2 rounded-md ${selectedInput === 'type2' ? 'bg-gradient-to-r from-primary via-green-500 to-lime-400 text-white' : 'bg-gray-200 text-gray-600'}`}
+          className={`px-6 py-2 rounded-md hover:scale-105 ${selectedInput === 'type2' ? 'bg-gradient-to-r from-primary via-green-500 to-lime-400 text-white' : 'bg-gray-200 text-gray-600'}`}
         >
           Advanced
         </button>
       </div>
       {selectedInput === 'type2' ? (
-        <div className="bg-red-400 text-center w-8/12 ml-10 items-centre">
+        <div className="bg-red-400 text-center w-8/12 ml-10 items-center">
           <p>use Select</p>
         </div>
       ) :
@@ -63,32 +68,63 @@ const InputTable = () => {
       }
       <div className="inputContainer">
         {isLoading ? (
-          // Show SkeletonLoader while loading
           <SkeletonLoader />
         ) : 
         selectedInput === 'type1' ? (
           <table className="inputTable">
             <thead>
-              <tr>
-                <th className="inputTabelLabel">Field</th>
-                <th className="inputTabelLabel">Input</th>
-              </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="inputTabelLabel">Name</td>
+                <td className="inputTabelLabel">Total Assets</td>
                 <td className="numberInputtd">
                   <InputField />
                 </td>
               </tr>
               <tr>
-                <td className="inputTabelLabel">Email</td>
+                <td className="inputTabelLabel">Current Assets</td>
                 <td className="numberInputtd">
                   <InputField />
                 </td>
               </tr>
               <tr>
-                <td className="inputTabelLabel border-b-0">Password</td>
+                <td className="inputTabelLabel">Cash & Cash Equivalents</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>              
+              <tr>
+                <td className="inputTabelLabel">Total Liabilities</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Current Liabilities</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Long-Term Debt</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Shareholders' Equity</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Revenue</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel border-b-0">Net Income</td>
                 <td className="numberInputtd border-b-0">
                   <InputField />
                 </td>
@@ -98,27 +134,107 @@ const InputTable = () => {
         ) : (
           <table className="inputTable">
             <thead>
-              <tr>
-                <th className="inputTabelLabel">asdasd</th>
-                <th className="inputTabelLabel">Input</th>
-              </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="inputTabelLabel">Name</td>
+                <td className="inputTabelLabel">Total Assets</td>
                 <td className="numberInputtd">
                   <InputField />
                 </td>
               </tr>
               <tr>
-                <td className="inputTabelLabel">Email</td>
+                <td className="inputTabelLabel">Current Assets</td>
                 <td className="numberInputtd">
                   <InputField />
                 </td>
               </tr>
               <tr>
-                <td className="inputTabelLabel border-b-0">Password</td>
-                <td className="numberInputtd border-b-0">
+                <td className="inputTabelLabel">Cash & Cash Equivalents</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>              
+              <tr>
+                <td className="inputTabelLabel">Total Liabilities</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Current Liabilities</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Long-Term Debt</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Shareholders' Equity</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Revenue</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Net Income</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Accounts Receivable</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Long-Term Debt</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Gross Profit</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Operating Income</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">COGS</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">SG&A</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel">Interest Expense</td>
+                <td className="numberInputtd">
+                  <InputField />
+                </td>
+              </tr>
+              <tr>
+                <td className="inputTabelLabel  border-b-0">Income Tax Expense</td>
+                <td className="numberInputtd  border-b-0">
                   <InputField />
                 </td>
               </tr>
